73a6d3dffdd3c768e167c2beae3b45f6096db618
Edward Dewhurst <edewhurst@tradingview.com>
2026-03-19 13:27:08 +0000

    Remove standalone.ts

--- src/standalone.ts
+++
@@ -1,4 +0,0 @@
-import * as LightweightChartsModule from './index';
-
-// put all exports from package to window.LightweightCharts object
-(window as any).LightweightCharts = LightweightChartsModule;
